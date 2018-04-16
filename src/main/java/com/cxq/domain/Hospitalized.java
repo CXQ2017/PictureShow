c6207a4f6d440aa258a3408cbba37da07ac0c896
CXQ2017 <269439340@qq.com>
2018-04-16 16:38:49 +0800

    实体类添加TEXT注解

--- src/main/java/com/cxq/domain/Hospitalized.java
+++ src/main/java/com/cxq/domain/Hospitalized.java
@@ -2,10 +2,7 @@ package com.cxq.domain;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.Date;
 
 /**
@@ -23,17 +20,25 @@ public class Hospitalized {
     @DateTimeFormat( pattern = "yyyy-MM-dd" )
     private Date record_day;//记录日期
     private String history_statement;//病史陈述者
+    @Column(columnDefinition="TEXT")
     private String main_suit;//入院主诉
+    @Column(columnDefinition="TEXT")
     private String present_illness_history;//现病史
+    @Column(columnDefinition="TEXT")
     private String anamnesis;//既往史
+
+    @Column(columnDefinition="TEXT")
     private String personal_history;//个人史
+    @Column(columnDefinition="TEXT")
     private String obsterical_history;//婚育史
+    @Column(columnDefinition="TEXT")
     private String family_history;//家族史
     private String temperature;//体格检查体温
     private String pulse;//脉搏
     private String breathe;//呼吸
     private String blood_pressure;//血压
     private Double weight;//体重
+    @Column(columnDefinition="TEXT")
     private String general_condition;//一般情况
     private String skin_mucosa;//皮肤黏膜
     private String lymphaden;//淋巴结
@@ -44,20 +49,26 @@ public class Hospitalized {
     private String anal_genitalia;//肛门生殖器
     private String spine;//脊柱四肢
     private String reflex;//神经生理行反射
+    @Column(columnDefinition="TEXT")
     private String special_case;//专科情况
+    @Column(columnDefinition="TEXT")
     private String accessory;//辅助检查
+    @Column(columnDefinition="TEXT")
     private String modified_diagnosis;//修正诊断
     private String modified_diagnosis_name;//修正诊断签名
     @DateTimeFormat( pattern = "yyyy-MM-dd" )
     private Date modified_diagnosis_day;//修正诊断日期
+    @Column(columnDefinition="TEXT")
     private String tentative_diagnosis;//初步诊断
     private String tentative_diagnosis_name;//初步诊断签名
     @DateTimeFormat( pattern = "yyyy-MM-dd" )
     private Date tentative_diagnosis_day;//初步诊断日期
+    @Column(columnDefinition="TEXT")
     private String supplementary_diagnosis;//补充诊断
     private String supplementary_diagnosis_name;//补充诊断签名
     @DateTimeFormat( pattern = "yyyy-MM-dd" )
     private Date supplementary_diagnosis_day;//补充诊断日期
+    @Column(columnDefinition="TEXT")
     private String final_diagnosis;//最后诊断
     private String final_diagnosis_name;//最后诊断签名
     @DateTimeFormat( pattern = "yyyy-MM-dd" )
