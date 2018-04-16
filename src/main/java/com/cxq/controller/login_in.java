acaf8a9ab9734715cd4658f04d64bf44dac3832c
CXQ2017 <269439340@qq.com>
2018-04-16 15:02:02 +0800

    冲突

--- src/main/java/com/cxq/controller/login_in.java
+++ src/main/java/com/cxq/controller/login_in.java
@@ -6,9 +6,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-import javax.servlet.http.HttpServletRequest;
 
 /**
  * Created by Administrator on 2018/4/11.
