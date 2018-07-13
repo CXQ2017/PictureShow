d3f92a5fec5958eae970bfb753fc1c256e73e8e2
CXQ2017 <269439340@qq.com>
2018-07-13 10:29:51 +0800

    updata

--- src/main/java/com/cxq/controller/AccessManagement.java
+++ src/main/java/com/cxq/controller/AccessManagement.java
@@ -112,40 +112,6 @@ public class AccessManagement {
             return "Access_management";
         }
 
-//    public static void pdfToImage(String pdfPath){
-//        try{
-//            if(pdfPath==null||"".equals(pdfPath)||!pdfPath.endsWith(".pdf"))
-//                return;
-//            //图像合并使用参数
-//            int width = 0; // 总宽度
-//            int[] singleImgRGB; // 保存一张图片中的RGB数据
-//            int shiftHeight = 0;
-//            BufferedImage imageResult = null;//保存每张图片的像素值
-//            //利用PdfBox生成图像
-//            PDDocument pdDocument = PDDocument.load(new File(pdfPath));
-//            PDFRenderer renderer = new PDFRenderer(pdDocument);
-//            //循环每个页码
-//            for(int i=0,len=pdDocument.getNumberOfPages(); i<len; i++){
-//                BufferedImage image=renderer.renderImageWithDPI(i, DEFAULT_DPI,ImageType.RGB);
-//                int imageHeight=image.getHeight();
-//                int imageWidth=image.getWidth();
-//                if(i==0){//计算高度和偏移量
-//                    width=imageWidth;//使用第一张图片宽度;
-//                    //保存每页图片的像素值
-//                    imageResult= new BufferedImage(width, imageHeight*len, BufferedImage.TYPE_INT_RGB);
-//                }else{
-//                    shiftHeight += imageHeight; // 计算偏移高度
-//                }
-//                singleImgRGB= image.getRGB(0, 0, width, imageHeight, null, 0, width);
-//                imageResult.setRGB(0, shiftHeight, width, imageHeight, singleImgRGB, 0, width); // 写入流中
-//            }
-//            pdDocument.close();
-//            File outFile = new File(pdfPath.replace(".pdf", "_"+DEFAULT_DPI+".jpg"));
-//            ImageIO.write(imageResult, "jpg", outFile);// 写图片
-//        }catch (Exception e) {
-//            e.printStackTrace();
-//        }
 
-//    }
 
 }
